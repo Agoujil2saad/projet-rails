55de76714cba3f3c00e0bcda584699202b11e5e9
agoujil <agoujilsaad@gmail.com>
2017-06-04 13:33:19 +0000

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+
+ 
+
 root 'pages#home'
 get 'inscription' => 'users#new' 
 get 'aide' => 'pages#aide'
 get 'contact' => 'pages#contact'
+
+ get 'login' => 'sessions#new'
+post 'login' => 'sessions#create'
+delete 'logout' => 'sessions#destroy'
+
 resources:users
 end
